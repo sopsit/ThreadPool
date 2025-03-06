3613b872b74915845bf8aa1f13d8d32affdb2894
sara <sararashedi@gmail.com>
2025-03-06 13:21:14 -0800

    fix bug

--- ThreadPool/main.go
+++ ThreadPool/main.go
@@ -11,10 +11,10 @@ func main() {
 	   a.PoolManager( "arriving_time1.txt" , "burst_time1.txt")
 
 
-	    b:=TreadPool.NewTredpool(5 , 10)
-         b.PoolManager( "arriving_time2.txt" , "burst_time2.txt")
+	//     b:=TreadPool.NewTredpool(5 , 10)
+    //      b.PoolManager( "arriving_time2.txt" , "burst_time2.txt")
 	
-	   c:=TreadPool.NewTredpool(8 , 5)
-	  c.PoolManager( "arriving_time3.txt" , "burst_time3.txt")
+	//    c:=TreadPool.NewTredpool(8 , 5)
+	//   c.PoolManager( "arriving_time3.txt" , "burst_time3.txt")
 
 }
